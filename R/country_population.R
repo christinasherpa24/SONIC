0a89e23b62aa33c083a569113ab1ed2adc7f2a53
Naomi Giancola <ngiancola@smith.edu>
2022-12-17 13:31:56 -0500

    NO MORE ERRORS!!!!!

--- R/country_population.R
+++ R/country_population.R
@@ -1,10 +1,11 @@
-globalVariables(c("country_territory", "population_2020", "population_2015", "population_2010", "population_2000"))
+globalVariables(c("country_territory", "population_2020", "population_2015", "population_2010", "population_2000", "world_population"))
+
 #' World Population (Between 2000-2022)
 #'@description this function takes in a a country/territory and returns the population of the country for the years 2000, 2010,2015 and 2022
 #' @param X is a country/territory that a user inputs of their choice
 #'
 #' @return a numeric vector that is the population of a country/territory for the years 2000, 2010,2015 and 2022
-#' @export
+#'
 #'
 #' @importFrom magrittr "%>%"
 #' @importFrom dplyr "filter"
@@ -17,7 +18,7 @@ globalVariables(c("country_territory", "population_2020", "population_2015", "po
 #'
 #' country_population("Afghanistan")
 #'
-#'
+#' @export
 #
 country_population <- function(X){
   population_user <- world_population %>%
